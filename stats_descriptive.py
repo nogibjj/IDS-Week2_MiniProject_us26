183c75fbc2657c566f2b22331e281a006ba02cb2
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-06 16:18:18 -0400

    Update stats_descriptive.py

--- stats_descriptive.py
+++ stats_descriptive.py
@@ -1,1 +1,29 @@
+import pandas as pd
+
+def stats(dataset):
+    mean_val = dataset.mean(numeric_only=True)
+    median_val = dataset.median(numeric_only=True)
+    mode_val = dataset.mode(numeric_only=True)
+    return mean_val, median_val, mode_val
+
+
+data = [
+    ["tom", 10],
+    ["nick", 15],
+    ["juli", 14],
+    ["Faizan", 10],
+    ["John", 15],
+    ["Kyran", 14],
+    ["Francis", 10],
+    ["Diego", 15],
+    ["Asad", 14],
+    ["Ollie", 10],
+    ["Rafael", 15],
+    ["Cecil", 14],
+]
+
+df = pd.DataFrame(data, columns=["Name", "Age"])
+
+if __name__ == "__main__":
+    answers = stats(df)
 
