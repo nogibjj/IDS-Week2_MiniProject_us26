3a6f55b87ad1c92e38a1a893f598ad34d96b3cf3
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-06 16:21:45 -0400

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,1 +1,8 @@
+import stats_descriptive
 
+def test_mean():
+    assert stats_descriptive.stats[0] == 2
+def test_median():
+    assert stats_descriptive.stats[1] == -1
+def test_mode():
+    assert stats_descriptive.stats[2] == 3
