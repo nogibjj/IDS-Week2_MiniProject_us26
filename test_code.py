c7a7850806e6ba6b807036dd0ea435de34447f1f
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-06 22:42:01 -0400

    Update test_code.py

--- test_code.py
+++ test_code.py
@@ -1,8 +1,24 @@
-import stats_descriptive
+from  stats_descriptive import stats_mean ,  stats_median ,  stats_mode
+import pandas as pd
 
-def test_mean():
-    assert stats_descriptive.stats[0] == 2
-def test_median():
-    assert stats_descriptive.stats[1] == -1
-def test_mode():
-    assert stats_descriptive.stats[2] == 3
+data = [
+    ["tom", 10],
+    ["nick", 15],
+    ["juli", 14],
+    ["Faizan", 10],
+    ["John", 15],
+    ["Kyran", 14],
+    ["Francis", 10],
+    ["Diego", 15],
+    ["Asad", 14],
+    ["Ollie", 10],
+    ["Rafael", 15],
+    ["Cecil", 15],
+]
+
+df = pd.DataFrame(data, columns=["Name", "Age"])
+
+if __name__ == "__main__":
+    assert (stats_mean(df)[0]) == 13.083333333333334
+    assert (stats_median(df)[0]) == 14.0
+    assert (stats_mode(df)["Age"][0]) == 15
