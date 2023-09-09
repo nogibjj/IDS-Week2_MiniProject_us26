0a6827855348c8c6e74a088bbd2d1cb734f0feea
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-09 19:07:24 -0400

    Update test_stats.py

--- test_stats.py
+++ test_stats.py
@@ -1,26 +1,16 @@
-from  stats_descriptive import stats_mean ,  stats_median ,  stats_mode
+from stats_descriptive import stats_mean, stats_median, stats_mode, stats_std
 import pandas as pd
 
-data = [
-    ["tom", 10],
-    ["nick", 15],
-    ["juli", 14],
-    ["Faizan", 10],
-    ["John", 15],
-    ["Kyran", 14],
-    ["Francis", 10],
-    ["Diego", 15],
-    ["Asad", 14],
-    ["Ollie", 10],
-    ["Rafael", 15],
-    ["Cecil", 15],
-]
+data = pd.read_csv("World University Rankings 2023.csv")
 
-df = pd.DataFrame(data, columns=["Name", "Age"])
+# Calculating values for "No of student per staff"
+
 
 def test_values():
-    assert (stats_mean(df)[0]) == 13.083333333333334
-    assert (stats_median(df)[0]) == 14.0
-    assert (stats_mode(df)["Age"][0]) == 15
+    assert (stats_mean(data)) == 16.724999999999998
+    assert (stats_median(data)) == 13.5
+    assert (stats_mode(data)[0]) == 10.3
+    assert (stats_std(data)) == 10.557828968365255
+
 
 test_values()
