6372ac19ec2c9a51d7de6a25b41c1a43c43005de
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-10 04:39:11 -0400

    Update data_visualization.py

--- data_visualization.py
+++ data_visualization.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import plotly.express as px
 
+data = pd.read_csv("World University Rankings 2023.csv")
+
+result  = data.groupby(['Location'])['University Rank'].count().reset_index()
+result1  = data.groupby(['Location'])['Industry Income Score'].mean().reset_index()
+merge_data = pd.merge(result, result1)
+
+fig = px.scatter(merge_data, x="Industry Income Score", y="University Rank", color="Location",
+                 size='University Rank')
+fig.update_layout(
+    title="Analysing Top Universities",
+    xaxis_title="Mean of Industry Income Score",
+    yaxis_title="Count of Top Universities")
+fig.show()
