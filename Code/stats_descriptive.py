2a9af18a8c0f5729e7a5309f4a21de1c69b63ca8
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-11 20:31:02 -0400

    Create stats_descriptive.py

---
+++ Code/stats_descriptive.py
@@ -0,0 +1,14 @@
+def stats_mean(dataset):
+    return dataset["No of student per staff"].mean()
+    
+
+def stats_median(dataset):
+    return dataset["No of student per staff"].median()
+
+
+def stats_mode(dataset):
+    return dataset["No of student per staff"].mode()
+
+
+def stats_std(dataset):
+    return dataset["No of student per staff"].std()
