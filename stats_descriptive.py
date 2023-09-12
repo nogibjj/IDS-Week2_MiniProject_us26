e5d18c831566fec323c990e883c02a055b3902ab
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-12 00:56:46 -0400

    Update stats_descriptive.py

--- stats_descriptive.py
+++ stats_descriptive.py
@@ -24,8 +24,7 @@ def report(data):
 
     return result
 
-def create_summary(summary = report, file_path = 'Generated summary report.md'):
+def create_summary(data, file_path = 'Generated summary report.md'):
     with open(file_path, 'w', encoding="utf-8") as f: 
-        for key, value in summary.items(): 
+        for key, value in report(data).items(): 
             f.write('%s:%s\n' % (key, value))
-            f.write('/n')
