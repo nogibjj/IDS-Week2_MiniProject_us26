b4e052255521f4cb5dfc8eda4deb2ab6fa28abdc
udyansachdev1 <143528446+udyansachdev1@users.noreply.github.com>
2023-09-12 03:55:10 +0000

    added summary.md file

--- stats_descriptive.py
+++ stats_descriptive.py
@@ -12,3 +12,20 @@ def stats_mode(dataset):
 
 def stats_std(dataset):
     return dataset["No of student per staff"].std()
+
+
+def summary(data):
+
+    summary = {
+        "mean": (stats_mean(data)),
+        "median": (stats_median(data)),
+        "std_dev": (stats_std(data)),
+        "mode":((stats_mode(data)[0]))
+    }
+
+    return summary
+
+def create_summary(summary, file_path = 'Generated summary report.md'):
+    with open(file_path, 'w') as f: 
+        for key, value in summary.items(): 
+            f.write('%s:%s\n' % (key, value))
